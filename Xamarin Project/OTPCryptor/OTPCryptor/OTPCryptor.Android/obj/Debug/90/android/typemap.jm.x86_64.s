b89a22bb23e12148f2bc1a464158cd25fa834c82
Abdul Basit Khan <itsabk@outlook.com>
2019-09-01 02:02:48 +0400

    Added base for flutter and xamarin projects

---
+++ Xamarin Project/OTPCryptor/OTPCryptor/OTPCryptor.Android/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 9ccb07e6abdb5137011f74b943979f880fdc1a91 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
