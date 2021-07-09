317dc3005ed54ca02fa899d2dd6ef202d23da371
Abdul Basit Khan <itsabk@outlook.com>
2021-07-09 18:47:41 +0400

    Restructured and updated to Unity 2020.3.12f1

---
+++ UnityProject/Assets/Scripts/CopyText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CopyText : MonoBehaviour
+{
+    public Text objText;
+    string textVal;
+
+
+    public void CopyToClipboard()
+    {
+        textVal = objText.text.ToString();
+        UniClipboard.SetText(textVal);
+        // text.CopyToClipboard();
+    }
+}
